ed4d0db364b41dc62ded8950d534d109abb145f7
hayley-d <hayleydod@proton.me>
2024-11-17 08:43:25 +0200

    struct practice

--- rust-concepts/src/main.rs
+++ rust-concepts/src/main.rs
@@ -1,4 +1,78 @@
+#[derive(Debug)]
+struct User {
+    username: String,
+    email: String,
+    age: u32,
+    online: bool,
+}
+
+impl Rectangle {
+    fn area_of_rectangle(&self) -> f32 {
+        return self.width * self.height;
+    }
+
+    pub fn can_contain(&self, other: &Rectangle) -> bool {
+        return self.area_of_rectangle() >= other.area_of_rectangle();
+    }
+}
+
+impl Rectangle {
+    fn square(size: f32) -> Rectangle {
+        return Rectangle {
+            width: size,
+            height: size,
+        };
+    }
+}
+
 fn main() {
+    let mut user1: User = User {
+        username: String::from("Hayley"),
+        email: String::from("hayleydod@proton.me"),
+        age: 22,
+        online: true,
+    };
+
+    let username: String = user1.username;
+    user1.username = String::from("haylo");
+
+    let user2: User = user_factory(String::from("Sloth"), String::from("email@gamil.com"), 8);
+
+    struct Colour(i32, i32, i32);
+    struct Point(i32, i32, i32);
+    let rec: Rectangle = Rectangle {
+        width: 30.5,
+        height: 2.5,
+    };
+
+    println!("{:#?}", user1);
+    println!("{}", rec.area_of_rectangle());
+
+    let square: Rectangle = Rectangle::square(7.5);
+    println!(
+        "The widht and height of the square is {}cm in length",
+        square.width
+    );
+}
+
+fn user_factory(username: String, email: String, age: u32) -> User {
+    return User {
+        username,
+        email,
+        age,
+        online: false,
+    };
+}
+
+struct Rectangle {
+    width: f32,
+    height: f32,
+}
+
+/*fn area_of_rectangle(rec: Rectangle) -> f32 {
+    return rec.width * rec.height;
+}*/
+fn main2() {
     // A 32-bit signed integer (immutable)
     let x: i32 = 7;
     println!("The value of x is {}", x);
--- rust-concepts/src/other_main.rs
+++ rust-concepts/src/other_main.rs
@@ -1,8 +1,19 @@
+#[derive(Debug)]
 struct User {
     username: String,
     email: String,
     age: u32,
     online: bool,
+}
+
+impl Rectangle {
+    pub fn area_of_rectangle(&self) -> f32 {
+        return self.width * self.height;
+    }
+
+    pub fn can_contain(&self, other: &Rectangle) -> bool {
+        return self.area_of_rectangle() >= other.area_of_rectangle();
+    }
 }
 
 fn main() {
@@ -20,6 +31,13 @@ fn main() {
 
     struct Colour(i32, i32, i32);
     struct Point(i32, i32, i32);
+    let rec: Rectangle = Rectangle {
+        width: 30.5,
+        height: 2.5,
+    };
+
+    println!("{:#?}", user1);
+    println!("{}", rec.area_of_rectangle());
 }
 
 fn user_factory(username: String, email: String, age: u32) -> User {
@@ -36,6 +54,6 @@ struct Rectangle {
     height: f32,
 }
 
-fn area_of_rectangle(rec: Rectangle) -> f32 {
+/*fn area_of_rectangle(rec: Rectangle) -> f32 {
     return rec.width * rec.height;
-}
+}*/
