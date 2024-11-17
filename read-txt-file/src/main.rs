0f05fadc4c8c665c4b65dccceb94a71ebb0d4171
hayley-d <hayleydod@proton.me>
2024-11-17 18:06:47 +0200

    adding file reads

--- read-txt-file/src/main.rs
+++ read-txt-file/src/main.rs
@@ -18,3 +18,25 @@ fn main() {
     };
     //file goes out of scope and the file is closed
 }
+
+fn skip_two_take_two() {
+    // I know unwrap is bad but this is trival for practice so leave me alone
+    let file = std::fs::read_to_string("lines").unwrap();
+    file.lines()
+        .enumerate()
+        .filter(|(index, content)| index % 2 == 0)
+        .skip(2)
+        .take(2)
+        .for_each(|(_, line)| print!("{} ", line));
+}
+
+fn skip_two_lines() {
+    // I know unwrap is bad but this is trival for practice so leave me alone
+    let file = std::fs::read_to_string("lines").unwrap();
+    file.lines()
+        .enumerate()
+        .filter(|(index, content)| index % 2 == 0)
+        .skip(2)
+        .take(2)
+        .for_each(|(_, line)| print!("{} ", line));
+}
