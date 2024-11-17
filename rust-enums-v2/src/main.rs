2b4ddb4459735294e4579cee223ef825370a59f4
hayley-d <hayleydod@proton.me>
2024-11-18 01:20:56 +0200

    messing with enums

--- rust-enums-v2/src/main.rs
+++ rust-enums-v2/src/main.rs
@@ -2,6 +2,27 @@ enum Colours {
     Red,
     Green,
     Blue,
+    Yellow,
+}
+
+struct Person {
+    age: i32,
+    name: String,
+}
+
+struct Employee {
+    person: Person,
+    job: String,
+}
+
+impl Employee {
+    fn print(&self) {
+        println!("Hi I am {} and I work as a {}.", self.person.name, self.job);
+    }
+
+    fn append(&mut self) {
+        self.person.name.push_str("Sloths");
+    }
 }
 
 impl Colours {
@@ -9,11 +30,47 @@ impl Colours {
         match self {
             Colours::Red => println!("Red"),
             Colours::Green => println!("Green"),
-            _ => println!("Blue"),
+            Colours::Blue => println!("Blue"),
+            _ => println!("Yellow"),
         };
     }
+
+    fn is_green(&self) -> bool {
+        if let Colours::Green = self {
+            return true;
+        }
+        return false;
+    }
+
+    fn is_green_parts(&self) -> bool {
+        match self {
+            Colours::Red => false,
+            Colours::Green => true,
+            Colours::Blue => true,
+            Colours::Yellow => true,
+        }
+    }
 }
+
+fn append(employees: &mut Vec<Employee>) {
+    for emp in employees {
+        emp.append();
+    }
+}
+
 fn main() {
     let colour: Colours = Colours::Red;
     colour.print();
+
+    let colour2: Colours = Colours::Green;
+    colour2.print();
+
+    let colour3: Colours = Colours::Blue;
+    colour3.print();
+
+    let colour4: Colours = Colours::Yellow;
+    colour4.print();
+
+    let mut employees: Vec<Employee> = vec![];
+    append(&mut employees);
 }
