5542ad5160aa589fcf99c03554fe99da27df364b
hayley-d <hayleydod@proton.me>
2024-11-18 09:21:56 +0200

    fixed generics error

--- rust-generics/src/main.rs
+++ rust-generics/src/main.rs
@@ -1,10 +1,15 @@
 fn main() {
     let number_list: Vec<i32> = vec![1, 2, 3, 4, 5, 6];
 
+    let max: i32 = match get_max_element(&number_list) {
+        Some(n) => n,
+        None => 0,
+    };
+
     println!("The vector is {:#?}", number_list);
-    println!("The largest number in the vector is {}", largest);
+    println!("The largest number in the vector is {}", max);
 }
 
 fn get_max_element<T: Ord + Copy>(list_of_items: &Vec<T>) -> Option<T> {
-    return list_of_items.iter().max();
+    return list_of_items.iter().max().copied();
 }
