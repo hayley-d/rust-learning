0dd61cdd437a26e759e181cc25ab174d6b5fa6c7
hayley-d <hayleydod@proton.me>
2024-11-21 15:41:43 +0200

    added my own error type

--- mini-grep/src/main.rs
+++ mini-grep/src/main.rs
@@ -1,7 +1,27 @@
 use std::env;
+use std::error::Error;
+use std::fmt;
 use std::fs;
-use std::io::{Error, ErrorKind};
 use std::process;
+
+#[derive(Debug)]
+pub enum MyError {
+    InvalidFile,
+    InvalidArgs,
+}
+
+impl Error for MyError {}
+
+impl fmt::Display for MyError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            MyError::InvalidFile => write!(f, "An error occured while attempting to read the file"),
+            MyError::InvalidArgs => {
+                write!(f, "This fuction needs two arguments query and file name")
+            }
+        }
+    }
+}
 
 fn main() {
     // args returns an iterator over the arguemnts passed to the program through the command line
@@ -14,14 +34,13 @@ fn main() {
             process::exit(1);
         }
     };
+}
 
-    let contents: String = match read_file_to_string(&query.file_name) {
-        Ok(s) => s,
-        Err(e) => {
-            println!("Error: {}", e);
-            process::exit(1);
-        }
-    };
+pub fn run(query: Query) -> Result<(), Box<dyn Error>> {
+    let contents: String = read_file_to_string(&query.file_name)?;
+
+    println!("The file contains: {}", &contents);
+    return Ok(());
 }
 
 pub struct Query<'a> {
@@ -30,25 +49,15 @@ pub struct Query<'a> {
 }
 
 impl<'a> Query<'a> {
-    fn new(args: &Vec<String>) -> Result<Query, Error> {
+    fn new(args: &Vec<String>) -> Result<Query, MyError> {
         let query: &str = match &args.get(1) {
             Some(v) => v,
-            None => {
-                return Err(Error::new(
-                    ErrorKind::Other,
-                    "Not enough arguments provided",
-                ))
-            }
+            None => return Err(MyError::InvalidArgs),
         };
 
         let file_name: &str = match &args.get(2) {
             Some(v) => v,
-            None => {
-                return Err(Error::new(
-                    ErrorKind::Other,
-                    "Not enough arguments provided",
-                ));
-            }
+            None => return Err(MyError::InvalidArgs),
         };
 
         println!("Searching for {}", query);
@@ -58,12 +67,9 @@ impl<'a> Query<'a> {
     }
 }
 
-pub fn read_file_to_string(file_name: &str) -> Result<String, Error> {
+pub fn read_file_to_string(file_name: &str) -> Result<String, MyError> {
     return match fs::read_to_string(&file_name) {
         Ok(s) => Ok(s),
-        Err(e) => Err(Error::new(
-            ErrorKind::Other,
-            "Failed to read contents of the file",
-        )),
+        Err(_) => Err(MyError::InvalidFile),
     };
 }
