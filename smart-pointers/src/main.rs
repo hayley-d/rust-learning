dd518ce45af51e6047ee2f2f2e74f0bbbbe9e87f
hayley-d <hayleydod@proton.me>
2024-11-23 16:36:59 +0200

    Learned defre trait

--- smart-pointers/src/main.rs
+++ smart-pointers/src/main.rs
@@ -18,6 +18,8 @@ struct Node {
     value: i32,
 }
 
+use std::ops::Deref;
+
 use List::{Cons, Nil};
 
 fn main() {
@@ -25,4 +27,48 @@ fn main() {
     // no overhead besides storing a value on the heap
     // not a fat pointer
     let list: List = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+}
+
+// The defef trait allows you to derefrence the pointers
+
+fn foo() {
+    let x: i32 = 5;
+    let y: &i32 = &x;
+    // y is a pointer to x
+    // you need to derefrence y to get the value pointed to by y
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let y = Box::new(x);
+    assert_eq!(5, *y);
+
+    // now y points to a copy of x because ints are copied when ownership
+
+    let y = MyBox::new(x);
+    assert_eq!(5, *y);
+
+    let name: MyBox<String> = MyBox::new(String::from("Hayley"));
+
+    say_hi(&name);
+    // &MyBox<String> -> &String basically equivalent to &str if deref is called
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        return MyBox(x);
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        return &self.0;
+    }
+}
+
+fn say_hi(name: &str) {
+    println!("Hello, {}!", name);
 }
