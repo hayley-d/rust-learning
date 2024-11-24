08eba840c5f1c24eab833a1effd007aa00871bb4
hayley-d <hayleydod@proton.me>
2024-11-24 16:18:42 +0200

    i did itgit add .!

--- rustTree/src/main.rs
+++ rustTree/src/main.rs
@@ -5,9 +5,9 @@ use std::rc::{Rc, Weak};
 #[derive(Debug)]
 struct Node {
     pub value: i32,
-    pub left: Option<RefCell<Rc<Node>>>,
-    pub right: Option<RefCell<Rc<Node>>>,
-    pub parent: Option<RefCell<Weak<Node>>>,
+    pub left: Option<Rc<RefCell<Node>>>,
+    pub right: Option<Rc<RefCell<Node>>>,
+    pub parent: Option<Weak<RefCell<Node>>>,
 }
 
 impl Node {
@@ -17,6 +17,28 @@ impl Node {
             left: None,
             right: None,
             parent: None,
+        };
+    }
+
+    pub fn set_parent(&mut self, parent: Weak<RefCell<Node>>) {
+        self.parent = Some(parent);
+    }
+
+    pub fn set_left(&mut self, child: Option<Rc<RefCell<Node>>>) {
+        self.left = child
+    }
+
+    pub fn set_right(&mut self, child: Option<Rc<RefCell<Node>>>) {
+        self.right = child
+    }
+
+    pub fn get_parent(&mut self) -> Rc<RefCell<Node>> {
+        return match &self.parent {
+            Some(p) => match p.upgrade() {
+                Some(p) => p,
+                None => panic!("This node has no parent"),
+            },
+            None => panic!("This node has no parent"),
         };
     }
 }
@@ -35,22 +57,26 @@ impl DerefMut for Node {
     }
 }
 
-pub fn add_child(parent: &RefCell<Rc<Node>>, child: &RefCell<Rc<Node>>) {
+pub fn add_child(parent: &Rc<RefCell<Node>>, child: Rc<RefCell<Node>>) {
     if child.borrow().value > parent.borrow().value {
-        parent.borrow_mut().right = Some(RefCell::new(Rc::clone(&child.borrow())));
-        child.borrow_mut().parent = Some(RefCell::new(Rc::downgrade(&parent.borrow())));
+        parent.borrow_mut().right = Some(Rc::clone(&child));
     } else {
-        parent.borrow_mut().left = Some(RefCell::new(Rc::clone(&child.borrow())));
-        child.borrow_mut().parent = Some(RefCell::new(Rc::downgrade(&parent.borrow())));
+        parent.borrow_mut().left = Some(Rc::clone(&child));
     }
+    child.borrow_mut().set_parent(Rc::downgrade(parent));
 }
 
 fn main() {
-    let root = Rc::new(Node::new(7));
+    let root = Rc::new(RefCell::new(Node::new(7)));
 
-    let first_leaf = Rc::new(Node::new(1));
+    let first_leaf = Rc::new(RefCell::new(Node::new(1)));
 
-    let second_leaf = Rc::new(Node::new(8));
+    let second_leaf = Rc::new(RefCell::new(Node::new(8)));
+
+    add_child(&Rc::clone(&root), Rc::clone(&first_leaf));
+    println!("The parent is {:?}", *first_leaf.borrow_mut().get_parent());
+    add_child(&Rc::clone(&root), Rc::clone(&second_leaf));
+    println!("The parent is {:?}", *second_leaf.borrow_mut().get_parent());
 }
 
 #[cfg(test)]
@@ -59,20 +85,16 @@ mod tests {
 
     #[test]
     fn test_nodes() {
-        let root = Rc::new(Node::new(7));
+        let root = Rc::new(RefCell::new(Node::new(7)));
 
-        let first_leaf = Rc::new(Node::new(1));
+        let first_leaf = Rc::new(RefCell::new(Node::new(1)));
 
-        let second_leaf = Rc::new(Node::new(8));
+        let second_leaf = Rc::new(RefCell::new(Node::new(8)));
 
-        add_child(
-            &RefCell::new(Rc::clone(&root)),
-            &RefCell::new(Rc::clone(&first_leaf)),
-        );
+        add_child(&Rc::clone(&root), Rc::clone(&first_leaf));
+        add_child(&Rc::clone(&root), Rc::clone(&second_leaf));
 
-        add_child(
-            &RefCell::new(Rc::clone(&root)),
-            &RefCell::new(Rc::clone(&second_leaf)),
-        );
+        assert_eq!(1, first_leaf.borrow().value);
+        assert_eq!(8, second_leaf.borrow().value);
     }
 }
